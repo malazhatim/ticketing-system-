5dcd8d4c7a0d51fc628d1c6044caa736c26d8058
malazhatim <malaz.hatim@gmail.com>
2024-06-26 07:39:59 -0700

    3

---
+++ delete_user.php
@@ -0,0 +1,21 @@
+<?php
+
+require "connection/connection.php";
+if(isset($_GET['uid'])){
+    $UserID = $_GET['uid'];
+    $check_user_tickets = "SELECT * FROM tickets WHERE TicketHandleBy=$UserID AND TicketStatus = 2";
+    $check_user_tickets_query = mysqli_query($conn,$check_user_tickets);
+    if(mysqli_num_rows($check_user_tickets_query)==0){
+        $delete_user = "UPDATE users SET Deleted = 1 WHERE UserID =$UserID";
+        $delete_user_query = mysqli_query($conn,$delete_user);
+        if($delete_user_query){
+            header('location:users.php?fb=4');
+        }
+        
+    }
+    else{
+        header('location:users.php?fb=9');
+    }
+}
+
+?>
